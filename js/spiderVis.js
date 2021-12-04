0cce5688c7416e59aa1aad37735fedf521f3f4ae
cindytian <55060072+ctian5136@users.noreply.github.com>
2021-12-04 11:47:06 -0500

    toggle working

--- js/spiderVis.js
+++ js/spiderVis.js
@@ -8,9 +8,11 @@ class SpiderVis {
         this.parentElement = parentElement;
         this.data = _data;
         this.displayData = [];
+        this.change = false;
 
         this.initVis()
     }
+
 
     initVis() {
         let vis = this;
@@ -55,7 +57,6 @@ class SpiderVis {
         for (var country in countryInfo) {
             vis.sorted.push([country, countryInfo[country]]);
         }
-        console.log("SORTED", vis.sorted);
 
         vis.sorted.sort(function(a, b) {
             return b[1] - a[1];
@@ -88,103 +89,20 @@ class SpiderVis {
             .attr("stroke", "gray");
 
         vis.radialScale = d3.scaleLinear()
-            .domain([0, 2000])
+            .domain([0, 1500])
             .range([0, 250]);
 
         // append the ticks for the count TO BE CHANGED
-        vis.ticks = [400, 800, 1200, 1600, 2000];
-
-        vis.ticks.forEach(t =>
-            vis.svg.append("circle")
-                .attr("cx", 300)
-                .attr("cy", 300)
-                .attr("fill", "none")
-                .attr("stroke", "gray")
-                .attr("r", vis.radialScale(t))
-        );
-
-        vis.ticks.forEach(t =>
-            vis.svg.append("text")
-                .attr("x", 305)
-                .attr("y",  300-vis.radialScale(t))
-                .text(t.toString())
-        );
-
-        // sets the labels as the nationalities and spaces them evenly across the chart
-        function angleToCoordinate(angle, value) {
-            let x = Math.cos(angle) * vis.radialScale(value);
-            let y = Math.sin(angle) * vis.radialScale(value);
-            return {"x": 300 + x, "y": 300 - y};
-        }
-        var value_line = d3.max(vis.ticks);
-        var value_label = value_line * 1.05;
-
-        for (var i = 0; i < 8; i++) {
-            let ft_name = vis.countries[i];
-            let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
-            let line_coordinate = angleToCoordinate(angle, value_line);
-            let label_coordinate = angleToCoordinate(angle, value_label);
-
-            //draw axis line
-            vis.svg.append("line")
-                .attr("x1", 300)
-                .attr("y1", 300)
-                .attr("x2", line_coordinate.x)
-                .attr("y2", line_coordinate.y)
-                .attr("stroke", "gray");
-
-            //draw axis label
-            vis.svg.append("text")
-                .attr("x", label_coordinate.x)
-                .attr("y", label_coordinate.y)
-                .attr("stroke", "black")
-                .attr("font-size", "10")
-
-                .text(ft_name);
-        }
-
-        // plotting the data
-        vis.line = d3.line()
-            .x(d => d.x)
-            .y(d => d.y);
-        vis.colors = ["pink"];
-
-        console.log("NAT", vis.nationality);
-
-        function getPathCoordinates(data_point) {
-            let coordinates = [];
-            for (var i = 0; i < 8; i++) {
-                let ft_name = vis.countries[i];
-                console.log("ft_name", ft_name);
-                let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
-                coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
-            }
-            return coordinates;
-        }
-        for (var i = 0; i < 1 ; i++) {
-            let d = vis.natData[i];
-            let color = vis.colors[i];
-            let coordinates = getPathCoordinates(d);
-            console.log("coord", coordinates);
+        vis.ticks = [300, 600, 900, 1200, 1500];
 
 
-            //     //draw the path element
-                vis.svg.append("path")
-                    .datum(coordinates)
-                    .attr("d", vis.line)
-                    .attr("stroke-width", 3)
-                    .attr("stroke", color)
-                    .attr("fill", color)
-                    .attr("stroke-opacity", 1)
-                    .attr("opacity", 0.5);
-            }
-            this.wrangleData();
-        }
+        this.wrangleData();
+    }
 
         wrangleData(){
-        let vis = this;
+            let vis = this;
             // INCLUDING AMERICANS
-            d3.select('#american-toggle').on('click', function() {
+            d3.select("#american-toggle").on("click", function (){
                 vis.natData = [];
                 vis.countries = [];
                 vis.sortable = vis.sorted.slice(0,8);
@@ -203,15 +121,34 @@ class SpiderVis {
 
                 console.log("Nationality array", vis.natData);
 
+                vis.svgNew = d3.select("#spidervis").append("svg")
+                    .attr("width", 800)
+                    .attr("height", 800)
+                    .attr("stroke", "gray");
+
                 vis.radialScale
                     .domain([0, 15000])
-                    .range([0, 15750]);
+                    .range([0, 250]);
 
                 // append the ticks for the count TO BE CHANGED
                 vis.ticks = [3000, 6000, 9000, 12000, 15000];
+                
+                vis.change = true;
 
+                vis.updateVis();
+
+            })
+            vis.updateVis();
+
+        }
+
+        updateVis() {
+            let vis = this;
+
+            if(this.change === false) {
                 vis.ticks.forEach(t =>
-                    vis.svg.append("circle")
+                    vis.svg
+                        .append("circle")
                         .attr("cx", 300)
                         .attr("cy", 300)
                         .attr("fill", "none")
@@ -220,7 +157,101 @@ class SpiderVis {
                 );
 
                 vis.ticks.forEach(t =>
-                    vis.svg.append("text")
+                    vis.svg
+                        .append("text")
+                        .attr("x", 305)
+                        .attr("y", 300 - vis.radialScale(t))
+                        .text(t.toString())
+                );
+
+                // sets the labels as the nationalities and spaces them evenly across the chart
+                function angleToCoordinate(angle, value) {
+                    let x = Math.cos(angle) * vis.radialScale(value);
+                    let y = Math.sin(angle) * vis.radialScale(value);
+                    return {"x": 300 + x, "y": 300 - y};
+                }
+
+                var value_line = d3.max(vis.ticks);
+                console.log(value_line);
+                var value_label = value_line * 1.05;
+
+                for (var i = 0; i < 8; i++) {
+                    let ft_name = vis.countries[i];
+                    let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
+                    let line_coordinate = angleToCoordinate(angle, value_line);
+                    let label_coordinate = angleToCoordinate(angle, value_label);
+
+                    //draw axis line
+                    vis.svg
+                        .append("line")
+                        .attr("x1", 300)
+                        .attr("y1", 300)
+                        .attr("x2", line_coordinate.x)
+                        .attr("y2", line_coordinate.y)
+                        .attr("stroke", "gray");
+
+                    //draw axis label
+                    vis.svg
+                        .append("text")
+                        .attr("x", label_coordinate.x)
+                        .attr("y", label_coordinate.y)
+                        .attr("stroke", "black")
+                        .attr("font-size", "10")
+                        .text(ft_name);
+                }
+
+                // plotting the data
+                vis.line = d3.line()
+                    .x(d => d.x)
+                    .y(d => d.y);
+                vis.colors = ["pink"];
+
+                function getPathCoordinates(data_point) {
+                    let coordinates = [];
+                    for (var i = 0; i < 8; i++) {
+                        let ft_name = vis.countries[i];
+                        console.log("ft_name", ft_name);
+                        let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
+                        coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+                    }
+                    return coordinates;
+                }
+
+                for (var i = 0; i < 1; i++) {
+                    let d = vis.natData[i];
+                    let color = vis.colors[i];
+                    let coordinates = getPathCoordinates(d);
+                    console.log("coord", coordinates);
+
+                    //     //draw the path element
+                    vis.svg
+                        .append("path")
+                        .datum(coordinates)
+                        .attr("d", vis.line)
+                        .attr("stroke-width", 3)
+                        .attr("stroke", color)
+                        .attr("fill", color)
+                        .attr("stroke-opacity", 1)
+                        .attr("opacity", 0.5);
+                }
+            }
+
+            else{
+                vis.svg.remove();
+
+                vis.ticks.forEach(t =>
+                    vis.svgNew
+                        .append("circle")
+                        .attr("cx", 300)
+                        .attr("cy", 300)
+                        .attr("fill", "none")
+                        .attr("stroke", "gray")
+                        .attr("r", vis.radialScale(t))
+                );
+
+                vis.ticks.forEach(t =>
+                    vis.svgNew
+                        .append("text")
                         .attr("x", 305)
                         .attr("y",  300-vis.radialScale(t))
                         .text(t.toString())
@@ -237,14 +268,13 @@ class SpiderVis {
 
                 for (var i = 0; i < 8; i++) {
                     let ft_name = vis.countries[i];
-                    console.log("ft_name", ft_name);
-
                     let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
                     let line_coordinate = angleToCoordinate(angle, value_line);
                     let label_coordinate = angleToCoordinate(angle, value_label);
 
                     //draw axis line
-                    vis.svg.append("line")
+                    vis.svgNew
+                        .append("line")
                         .attr("x1", 300)
                         .attr("y1", 300)
                         .attr("x2", line_coordinate.x)
@@ -252,12 +282,12 @@ class SpiderVis {
                         .attr("stroke", "gray");
 
                     //draw axis label
-                    vis.svg.append("text")
+                    vis.svgNew
+                        .append("text")
                         .attr("x", label_coordinate.x)
                         .attr("y", label_coordinate.y)
                         .attr("stroke", "black")
                         .attr("font-size", "10")
-
                         .text(ft_name);
                 }
 
@@ -265,12 +295,13 @@ class SpiderVis {
                 vis.line = d3.line()
                     .x(d => d.x)
                     .y(d => d.y);
-                vis.colors = ["orange"];
+                vis.colors = ["pink"];
 
                 function getPathCoordinates(data_point) {
                     let coordinates = [];
                     for (var i = 0; i < 8; i++) {
                         let ft_name = vis.countries[i];
+                        console.log("ft_name", ft_name);
                         let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
                         coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
                     }
@@ -282,8 +313,10 @@ class SpiderVis {
                     let coordinates = getPathCoordinates(d);
                     console.log("coord", coordinates);
 
-                    //draw the path element
-                    vis.svg.append("path")
+
+                    //     //draw the path element
+                    vis.svgNew
+                        .append("path")
                         .datum(coordinates)
                         .attr("d", vis.line)
                         .attr("stroke-width", 3)
@@ -292,10 +325,9 @@ class SpiderVis {
                         .attr("stroke-opacity", 1)
                         .attr("opacity", 0.5);
                 }
-            })
+            }
 
 
-            // FOREIGN NATIONALITIES
         }
 
 
@@ -365,3 +397,88 @@ class SpiderVis {
     // updateVis(){
     //
 }
+
+
+//
+// vis.ticks.forEach(t =>
+//     vis.svg.append("circle")
+//         .attr("cx", 300)
+//         .attr("cy", 300)
+//         .attr("fill", "none")
+//         .attr("stroke", "gray")
+//         .attr("r", vis.radialScale(t))
+// );
+//
+// vis.ticks.forEach(t =>
+//     vis.svg.append("text")
+//         .attr("x", 305)
+//         .attr("y",  300-vis.radialScale(t))
+//         .text(t.toString())
+// );
+//
+// // sets the labels as the nationalities and spaces them evenly across the chart
+// function angleToCoordinate(angle, value) {
+//     let x = Math.cos(angle) * vis.radialScale(value);
+//     let y = Math.sin(angle) * vis.radialScale(value);
+//     return {"x": 300 + x, "y": 300 - y};
+// }
+// var value_line = d3.max(vis.ticks);
+// var value_label = value_line * 1.05;
+//
+// for (var i = 0; i < 8; i++) {
+//     let ft_name = vis.countries[i];
+//     console.log("ft_name", ft_name);
+//
+//     let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
+//     let line_coordinate = angleToCoordinate(angle, value_line);
+//     let label_coordinate = angleToCoordinate(angle, value_label);
+//
+//     //draw axis line
+//     vis.svg.append("line")
+//         .attr("x1", 300)
+//         .attr("y1", 300)
+//         .attr("x2", line_coordinate.x)
+//         .attr("y2", line_coordinate.y)
+//         .attr("stroke", "gray");
+//
+//     //draw axis label
+//     vis.svg.append("text")
+//         .attr("x", label_coordinate.x)
+//         .attr("y", label_coordinate.y)
+//         .attr("stroke", "black")
+//         .attr("font-size", "10")
+//
+//         .text(ft_name);
+// }
+//
+// // plotting the data
+// vis.line = d3.line()
+//     .x(d => d.x)
+//     .y(d => d.y);
+// vis.colors = ["orange"];
+//
+// function getPathCoordinates(data_point) {
+//     let coordinates = [];
+//     for (var i = 0; i < 8; i++) {
+//         let ft_name = vis.countries[i];
+//         let angle = (Math.PI / 2) + (2 * Math.PI * i / 8);
+//         coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+//     }
+//     return coordinates;
+// }
+// for (var i = 0; i < 1 ; i++) {
+//     let d = vis.natData[i];
+//     let color = vis.colors[i];
+//     let coordinates = getPathCoordinates(d);
+//     console.log("coord", coordinates);
+//
+//     //draw the path element
+//     vis.svg.append("path")
+//         .datum(coordinates)
+//         .attr("d", vis.line)
+//         .attr("stroke-width", 3)
+//         .attr("stroke", color)
+//         .attr("fill", color)
+//         .attr("stroke-opacity", 1)
+//         .attr("opacity", 0.5);
+// }
